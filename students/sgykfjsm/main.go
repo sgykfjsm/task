ffd1bc2953eebf4378232105e005cb796ba0d2e9
shigeyuki-fujishima <shigeyuki.fujishima@linecorp.com>
2018-02-02 15:51:35 +0900

    Added the function skeleton

--- students/sgykfjsm/main.go
+++ students/sgykfjsm/main.go
@@ -7,6 +7,45 @@ import (
 	"github.com/urfave/cli"
 )
 
+type Task struct {
+	ID          int
+	Description string
+	Finished    bool
+}
+
+type Storage interface {
+	Add(string)(int, error)
+	Do(int) error
+	List()
+}
+
+type FileStorage struct{
+	*os.File
+}
+
+func NewFileStorage(filePath string) (*FileStorage, error) {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &FileStorage{f}, nil
+}
+
+// Add adds new task. If succeeded, Add returns task ID with Nil. If not, Add returns error and its ID is zero
+func (f *FileStorage) Add(description string) (taskID int, err error) {
+	return
+}
+
+// Do mark the task specified with `taskID` as completed. If the task doesn't exist, Do return error.
+func (f *FileStorage) Do(taskID int) (err error) {
+	return
+}
+
+// List prints the list of TODOs unmarked as finished.
+func (f *FileStorage) List() {
+	return
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "task"
@@ -23,16 +62,16 @@ func main() {
 		},
 	}
 	doCommand := cli.Command{
-		Name:    "do",
-		Usage:   `Mark a task on your TODO list as complete`,
+		Name:  "do",
+		Usage: `Mark a task on your TODO list as complete`,
 		Action: func(c *cli.Context) error {
 			fmt.Println("do")
 			return nil
 		},
 	}
 	listCommand := cli.Command{
-		Name:    "list",
-		Usage:   `List all of your incomplete tasks`,
+		Name:  "list",
+		Usage: `List all of your incomplete tasks`,
 		Action: func(c *cli.Context) error {
 			fmt.Println("list")
 			return nil
